d63e4eff1c2878aae9aadcd2a6dcfdecaa405ae1
sophiexzhao <x.zhao27@gmail.com>
2019-01-04 16:57:59 -0500

    initial commit

---
+++ springboot-actuator/src/test/java/com/zhao/springbootactuator/SpringbootActuatorApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhao.springbootactuator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootActuatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
